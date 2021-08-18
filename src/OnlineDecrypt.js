bcb075c24857f7599874692a0925faeaae25d768
umidjon-2231 <83820489+umidjon-2231@users.noreply.github.com>
2021-08-18 13:01:08 +0500

    encrypt by url optimization

--- src/OnlineDecrypt.js
+++ src/OnlineDecrypt.js
@@ -9,11 +9,31 @@ const OnlineDecrypt = () => {
         console.log(key, text)
     })
     return (
-        <div>
+        <div className="container">
             <Helmet>
                 <meta name="description" content={decrypt(text, key)}/>
                 <title>{key}</title>
             </Helmet>
+            <h1 className="mt-5 text-center text-sm-left">Vijener code </h1>
+            <div className="col-sm col-10 mx-auto my-4 bg-info" style={{height: "2px"}}/>
+
+            <div className="row mt-5">
+                <div className="col-sm-4 col-12">
+                    <div className="card">
+                        <div className="card-body"><b>Key:</b> "{key}"</div>
+                    </div>
+                </div>
+                <div className="col-sm-4 col-12 mt-3 mt-sm-0">
+                    <div className="card">
+                        <div className="card-body"><b>Text:</b> "{text}"</div>
+                    </div>
+                </div>
+                <div className="col-sm-4 col-12 mt-3 mt-sm-0">
+                    <div className="card">
+                        <div className="card-body"><b>Encrypt:</b> "{decrypt(text, key)}"</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/OnlineEncrypt.js
+++ src/OnlineEncrypt.js
@@ -1,20 +1,38 @@
 import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom"
 import {Helmet} from "react-helmet"
-import {decrypt, encrypt} from "./tools"
+import { encrypt} from "./tools"
 
 const OnlineEncrypt = () => {
 
     let {key, text}=useParams()
-    useEffect(()=>{
-        console.log(key, text)
-    })
+
     return (
-        <div>
+        <div className="container">
             <Helmet>
                 <meta name="description" content={encrypt(text, key)}/>
                 <title>{key}</title>
             </Helmet>
+            <h1 className="mt-5 text-center text-sm-left">Vijener code </h1>
+            <div className="col-sm col-10 mx-auto my-4 bg-info" style={{height: "2px"}}/>
+
+            <div className="row mt-5">
+                <div className="col-sm-4 col-12">
+                    <div className="card">
+                        <div className="card-body"><b>Key:</b> "{key}"</div>
+                    </div>
+                </div>
+                <div className="col-sm-4 col-12 mt-3 mt-sm-0">
+                    <div className="card">
+                        <div className="card-body"><b>Text:</b> "{text}"</div>
+                    </div>
+                </div>
+                <div className="col-sm-4 col-12 mt-3 mt-sm-0">
+                    <div className="card">
+                        <div className="card-body"><b>Encrypt:</b> "{encrypt(text, key)}"</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
